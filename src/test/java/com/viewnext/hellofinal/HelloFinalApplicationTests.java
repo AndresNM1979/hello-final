58968ed8bb774376552927c1f77cec48a8ea81ad
Andres Natera <andres.natera@viewnext.com>
2021-08-13 08:24:04 +0200

    Se añade en HelloFinalApplicationTests unas funciones para crear el servicio (corregimos comillas en los imports)

--- src/test/java/com/viewnext/hellofinal/HelloFinalApplicationTests.java
+++ src/test/java/com/viewnext/hellofinal/HelloFinalApplicationTests.java
@@ -1,13 +1,10 @@
 package com.viewnext.hellofinal;
 
-
-import org.codehaus.groovy.transform.tailrec.GotoRecurHereException
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
