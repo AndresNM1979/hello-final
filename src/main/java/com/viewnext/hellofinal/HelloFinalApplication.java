c9897a98fd970fd73d661c8d1f57a2c22cdb9c79
Andres Natera <andres.natera@viewnext.com>
2021-08-13 15:40:59 +0200

    Se añaden archivos de comprovacion

--- src/main/java/com/viewnext/hellofinal/HelloFinalApplication.java
+++ src/main/java/com/viewnext/hellofinal/HelloFinalApplication.java
@@ -2,12 +2,24 @@ package com.viewnext.hellofinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloFinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloFinalApplication.class, args);
 	}
+	@GetMapping("/")
+	public String root(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 
+	@GetMapping("/despedida")
+	public String despedida() {
+		return "Esta es la practica de finalizacion del curso, hasta pronto compañeros";
+	}
 }
---
+++ src/main/java/com/viewnext/hellofinal/HomeController.java
@@ -0,0 +1,15 @@
+package com.viewnext.hellofinal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HomeController {
+
+    @RequestMapping("/hola")
+    public @ResponseBody String hola() {
+        return "Hola ke ase";
+    }
+
+}
