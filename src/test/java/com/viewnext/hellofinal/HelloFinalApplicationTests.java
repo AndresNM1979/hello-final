682ee8e642b058adf0dbb792b2749fc43e123739
Andres Natera <andres.natera@viewnext.com>
2021-08-13 07:51:11 +0200

    Se añade en HelloFinalApplicationTests unas funciones para crear el servicio.

--- src/test/java/com/viewnext/hellofinal/HelloFinalApplicationTests.java
+++ src/test/java/com/viewnext/hellofinal/HelloFinalApplicationTests.java
@@ -1,13 +1,29 @@
 package com.viewnext.hellofinal;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.codehaus.groovy.transform.tailrec.GotoRecurHereException
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
 class HelloFinalApplicationTests {
 
-	@Test
-	void contextLoads() {
+	static void main(String[] args) {
+		SpringApplication.run(HelloFinalApplicationTests.class, args);
 	}
 
+	@GetMapping("/")
+	public String root(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/Despedida")
+	public String Despedida() {
+		return "Esta es la practica de finalizacion del curso, hasta pronto compañeros";
+	}
 }
