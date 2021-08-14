5f22049b2ac6099afe385d7b70002012d06023e8
Andres Natera <andres.natera@viewnext.com>
2021-08-14 22:02:21 +0200

    Realizamos cambios de Java16

--- src/test/java/com/viewnext/hellofinal/HelloFinalApplicationTests.java
+++ src/test/java/com/viewnext/hellofinal/HelloFinalApplicationTests.java
@@ -1,26 +1,15 @@
 package com.viewnext.hellofinal;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-@RestController
+@SpringBootTest
 class HelloFinalApplicationTests {
 
-	static void main(String[] args) {
-		SpringApplication.run(HelloFinalApplicationTests.class, args);
+	@Test
+	void contextLoads() {
+
 	}
 
-	@GetMapping("/")
-	public String root(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
-	@GetMapping("/Despedida")
-	public String Despedida() {
-		return "Esta es la practica de finalizacion del curso, hasta pronto compañeros";
-	}
 }
+
---
+++ src/test/java/com/viewnext/hellofinal/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.viewnext.hellofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HomeControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void hola() {
+        assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/hola", String.class))
+                .isEqualTo("Hola, esta ha sido el final del curso.");
+
+    }
+}
